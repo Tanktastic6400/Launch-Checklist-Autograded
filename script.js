c688ebb4647b4ce28e136e13ac671d3628d0a2dc
Tanktastic6400 <159968548+Tanktastic6400@users.noreply.github.com>
2024-08-14 20:22:06 -0500

    failing tests because of pilotStatus?

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
+
+
 // Write your JavaScript code here!
-
-
-
 window.addEventListener("load", function () {
 
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
     let button = this.document.getElementById("formSubmit");
     let faultyItems = this.document.getElementById("faultyItems");
-    console.log(faultyItems.firstElementChild);
-    
+
+
     let pilot;
     let copilot;
     let fuelLevel;
     let cargoMass;
+    let planetInfo;
 
     button.addEventListener("click", function (event) {
         pilot = document.querySelector("input[name=pilotName]");
@@ -21,16 +21,20 @@ window.addEventListener("load", function () {
         fuelLevel = document.querySelector("input[name=fuelLevel]");
         cargoMass = document.querySelector("input[name=cargoMass]");
         event.preventDefault();
-        
+
         formSubmission(document, faultyItems, pilot.value, copilot.value, fuelLevel.value, cargoMass.value);
     });
-    
-    let listedPlanetsResponse;
+
+    let listedPlanetsResponse = myFetch();
     listedPlanetsResponse.then(function (result) {
         listedPlanets = result;
+
         console.log(listedPlanets);
     }).then(function () {
         console.log(listedPlanets);
         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+        planetInfo = pickPlanet(listedPlanets);
+        addDestinationInfo(document, planetInfo.name,  planetInfo.diameter,  planetInfo.star,  planetInfo.distance,  planetInfo.moons,  planetInfo.image);
+
     });
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -4,17 +4,20 @@ require('cross-fetch/polyfill');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
     // Here is the HTML formatting for our mission target div.
-    /*
+    let missionDiv = document.getElementById("missionTarget");
+
+    missionDiv.innerHTML = `
                  <h2>Mission Destination</h2>
                  <ol>
-                     <li>Name: </li>
-                     <li>Diameter: </li>
+                     <li>Name: ${name} </li>
+                     <li>Diameter: ${diameter}</li>
                      <li>Star: ${star}</li>
-                     <li>Distance from Earth: </li>
-                     <li>Number of Moons: </li>
+                     <li>Distance from Earth: ${distance} </li>
+                     <li>Number of Moons: ${moons} </li>
                  </ol>
-                 <img src="">
-    */
+                 <img src="${imageUrl}">
+                 `
+
 }
 
 function validateInput(testInput) {
@@ -60,15 +63,18 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then(function (response) {
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then(function (response) {
+        return response.json();
     });
-
+    console.log("This is planetsReturned: " + planetsReturned);
     return planetsReturned;
 }
 
 function pickPlanet(planets) {
-}
+    let selectedPlanet = planets[Math.floor(Math.random() * planets.length)];
 
+    return  selectedPlanet;
+}
 //this starts the supporting functions
 //check cargo mass
 function checkCargoMass(cargoMass, cargoStatus, cargoLevel, launchStatus, list, fuelLevel) {
